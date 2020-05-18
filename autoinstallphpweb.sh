3efccc9e0312b8493a1b32252a58c49570447659
xx007 <1134802336@qq.com>
2020-05-18 23:30:06 +0800

    初始化yum

---
+++ autoinstallphpweb.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+yum install -y	gcc gcc++ gcc-c++ cmake make libxml2 libxml2-devel \
+		openssl openssl-devel perl perl-devel autoconf lrzsz unzip \
+		pcre pcre-devel perl-ExtUtils-Embed ncurses-devel bison rsync \
+		vim-enhanced vim-minimal vim-common unzip libjpeg-devel libpng-devel freetype-devel \
+		ntpdate openssh-clients wget net-tools psmisc iptables-services
